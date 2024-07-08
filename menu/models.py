da6ef5fc7d1818705254183bd164f4a5bbfa94d2
Pcosby5 <pcosby50@gmail.com>
2024-07-08 14:36:10 +0200

    made changes to menu serializer

--- menu/models.py
+++ menu/models.py
@@ -11,6 +11,7 @@ class MenuItem(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.URLField(blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
     def __str__(self):
         return self.name
